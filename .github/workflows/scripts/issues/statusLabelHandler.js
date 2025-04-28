12d497f7654fe45bc7e296053ae357f1cfde53a7
michel <jsnerdic@gmail.com>
2025-04-28 15:15:19 +0200

    Fix comment author assignment for created issues in status label handler

--- .github/workflows/scripts/issues/statusLabelHandler.js
+++ .github/workflows/scripts/issues/statusLabelHandler.js
@@ -27,7 +27,7 @@ module.exports = async ({ core, context, github }) => {
 
     // there is no comment when the issue gets closed, so we just assign null and continue with the action
     const commentAuthor =
-      context.payload.action === 'closed' ? null : context.payload.comment.user.login;
+      context.payload.action === 'created' ? context.payload.comment.user.login : null;
     core.info(`>>> Comment author: ${commentAuthor}`);
 
     // return early if the author of the comment is not the same as the author of the issue
